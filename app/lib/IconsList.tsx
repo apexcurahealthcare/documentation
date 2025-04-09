648c36adbc3c1ba3983f5e1c661f7cbc321bb53e
charanvinay <charanvinaynarni@gmail.com>
2025-04-09 20:15:38 +0530

    seperation

--- app/lib/IconsList.tsx
+++ app/lib/IconsList.tsx
@@ -4,7 +4,10 @@ import React, { useEffect, useState, useMemo, useCallback } from "react";
 import toast, { Toaster } from "react-hot-toast";
 import { FiSearch } from "react-icons/fi";
 import { IoIosCheckmarkCircle } from "react-icons/io";
+import { TbIcons } from "react-icons/tb";
+import { FaImages } from "react-icons/fa";
 import RevealWrapper from "./Motion";
+import { Segmented } from "antd";
 
 declare global {
   interface Window {
@@ -12,12 +15,25 @@ declare global {
   }
 }
 
+const LIGHTICONS = [
+  "aci-leads-converted",
+  "aci-leads-discarded",
+  "aci-leads-total",
+  "aci-leads-qualified",
+  "aci-calls-answered",
+  "aci-calls-calledback",
+  "aci-calls-call-quality",
+  "aci-calls-missed",
+  "aci-calls-rounded",
+];
+
 const IconsList = () => {
   const [icons, setIcons] = useState<{ [key: string]: string }>({});
   const [search, setSearch] = useState("");
   const [debouncedSearch, setDebouncedSearch] = useState("");
   const [copied, setCopied] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [viewType, setViewType] = useState<"icon" | "image">("icon");
 
   useEffect(() => {
     const script = document.createElement("script");
@@ -44,17 +60,8 @@ const IconsList = () => {
     const handler = setTimeout(() => {
       setDebouncedSearch(search);
     }, 300);
-
     return () => clearTimeout(handler);
   }, [search]);
-
-  const filteredIcons = useMemo(() => {
-    return Object.entries(icons)
-      .filter(([className]) =>
-        className.toLowerCase().includes(debouncedSearch.toLowerCase())
-      )
-      .sort(([a], [b]) => a.localeCompare(b));
-  }, [icons, debouncedSearch]);
 
   const handleCopy = useCallback((className: string) => {
     navigator.clipboard.writeText(className).then(() => {
@@ -72,6 +79,64 @@ const IconsList = () => {
     });
   }, []);
 
+  const iconEntries = useMemo(() => {
+    const entries = Object.entries(icons);
+    const iconTypeEntries = entries.filter(
+      ([key]) => !LIGHTICONS.includes(key)
+    );
+    const imageTypeEntries = entries.filter(([key]) =>
+      LIGHTICONS.includes(key)
+    );
+    return { icon: iconTypeEntries, image: imageTypeEntries };
+  }, [icons]);
+
+  const filteredIconEntries = useMemo(() => {
+    return iconEntries.icon.filter(([className]) =>
+      className.toLowerCase().includes(debouncedSearch.toLowerCase())
+    );
+  }, [iconEntries, debouncedSearch]);
+
+  const filteredImageEntries = useMemo(() => {
+    return iconEntries.image.filter(([className]) =>
+      className.toLowerCase().includes(debouncedSearch.toLowerCase())
+    );
+  }, [iconEntries, debouncedSearch]);
+
+  const filteredIcons = useMemo(() => {
+    const current =
+      viewType === "icon" ? filteredIconEntries : filteredImageEntries;
+    return current.sort(([a], [b]) => a.localeCompare(b));
+  }, [filteredIconEntries, filteredImageEntries, viewType]);
+
+  const SEGMENTS = useMemo(
+    () => [
+      {
+        label: (
+          <div className="flex items-center gap-1">
+            <TbIcons />
+            <p className="font-medium">Icons ({filteredIconEntries.length})</p>
+          </div>
+        ),
+        value: "icon",
+      },
+      {
+        label: (
+          <div className="flex items-center gap-1">
+            <FaImages size={14} />
+            <p className="font-medium">
+              Images ({filteredImageEntries.length})
+            </p>
+          </div>
+        ),
+        value: "image",
+      },
+    ],
+    [filteredIconEntries, filteredImageEntries]
+  );
+
+  const totalFilteredCount =
+    filteredIconEntries.length + filteredImageEntries.length;
+
   return (
     <div className="space-y-4 relative">
       <RevealWrapper>
@@ -80,6 +145,7 @@ const IconsList = () => {
           title="Some icons may not be visible clearly below as they are in white color. Hover can help you."
         />
       </RevealWrapper>
+
       <RevealWrapper>
         <Input
           classNames={{
@@ -98,6 +164,19 @@ const IconsList = () => {
         />
       </RevealWrapper>
 
+      <RevealWrapper>
+        <div className="flex items-center justify-between">
+          <Segmented
+            options={SEGMENTS}
+            onChange={(value) => setViewType(value as "icon" | "image")}
+            value={viewType}
+          />
+          <p className="text-gray-800 font-medium text-sm">
+            {totalFilteredCount} items found
+          </p>
+        </div>
+      </RevealWrapper>
+
       <div className="grid grid-cols-5 sm:grid-cols-2 gap-2">
         {filteredIcons?.length > 0 ? (
           filteredIcons.map(([className, svg], i) => (
@@ -106,11 +185,21 @@ const IconsList = () => {
                 key={i}
                 className={`d-center gap-4 hover:shadow-lg cursor-pointer rounded-md border h-28 p-2 bg-primary/5 hover:bg-[#ffe4c4] transition-all ease-in-out duration-300 ${
                   copied === className ? "border-green-500 bg-green-100" : ""
+                } ${
+                  LIGHTICONS?.includes(className)
+                    ? "bg-sky-700 hover:bg-sky-600 text-white"
+                    : ""
                 }`}
                 onClick={() => handleCopy(className)}
               >
                 <span dangerouslySetInnerHTML={{ __html: svg }}></span>
-                <p className="text-[8px] text-center font-semibold text-gray-500">
+                <p
+                  className={`text-[8px] text-center font-semibold ${
+                    LIGHTICONS?.includes(className)
+                      ? "text-white"
+                      : "text-gray-500"
+                  }`}
+                >
                   {className}
                 </p>
               </div>
@@ -127,7 +216,7 @@ const IconsList = () => {
         ) : (
           <RevealWrapper>
             <p className="col-span-5 text-sm font-medium text-center text-gray-500">
-              No icons found
+              No {viewType === "icon" ? "icons" : "images"} found
             </p>
           </RevealWrapper>
         )}
