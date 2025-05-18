0eed83067356566b8a33fb24b2ebddf1a5d6dd15
charanvinay-ael <charanvinay@apexcura.com>
2025-05-18 23:58:11 +0530

    nextjs webpack setup removed

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,7 @@
-import path from "path";
-
 const nextConfig = {
   /* config options here */
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      react: path.resolve(__dirname, 'node_modules/react'),
-      'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
-    };
-    return config;
   },
 };
 
